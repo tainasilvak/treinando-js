48694b34403496b37b9772ada84d7998c4a9037d
tainasilvak <tainacunha42@gmail.com>
2023-08-25 13:32:43 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// # Módulo 2
+
+// 1. Crie uma página HTML com um botão e um parágrafo. Escreva um programa que, ao clicar no botão, altere o texto do parágrafo para "Olá, mundo!" utilizando o método getElementById.
+
+const button = document.getElementById('#buttonMapped')
+
+function myButton(){
+    alert("me chamou")
+}
+
+button.addEventListener('click', myButton)
+
+
+
+
